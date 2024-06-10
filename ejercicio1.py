820f8e9143387c0559269a232755d012836c55d2
esto ya no es tuyo <jguimaroro@educacion.navarra.es>
2024-06-10 18:55:09 +0200

    ejercicio1

---
+++ ejercicio1.py
@@ -0,0 +1,14 @@
+def saludar(nombre):
+    
+    '''Función que imprime un saludo personalizado con el nombre dado.
+
+    Parámetros de entrada:
+        -nombre: El nombre de la persona a saludar.
+
+    Salida:
+        -Devuelve el saludo ¡Hola nombre!
+    '''
+
+    print('¡Hola '+nombre+'!')
+
+saludar("Juan")
